5812a09f42ccecadbcde2e3ccb68bc975e976436
s-anzie <severinanzie@gmail.com>
2025-05-14 11:06:19 +0200

    certificates

--- example/reqres/client/reqres.go
+++ example/reqres/client/reqres.go
@@ -1,16 +1,11 @@
 package main
 
 import (
-	"crypto/rand"
-	"crypto/rsa"
 	"crypto/tls"
-	"crypto/x509"
-	"encoding/pem"
 	"errors"
 	"flag"
 	"fmt"
 	"io"
-	"math/big"
 	"os"
 	"strconv"
 	"strings"
@@ -169,25 +164,4 @@ type loggingWriter struct{ io.Writer }
 func (w loggingWriter) Write(b []byte) (int, error) {
 	fmt.Printf("Server: Got '%s'\n", string(b))
 	return w.Writer.Write(b)
-}
-
-// Setup a bare-bones TLS config for the server
-func generateTLSConfig() *tls.Config {
-	key, err := rsa.GenerateKey(rand.Reader, 1024)
-	if err != nil {
-		panic(err)
-	}
-	template := x509.Certificate{SerialNumber: big.NewInt(1)}
-	certDER, err := x509.CreateCertificate(rand.Reader, &template, &template, &key.PublicKey, key)
-	if err != nil {
-		panic(err)
-	}
-	keyPEM := pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(key)})
-	certPEM := pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: certDER})
-
-	tlsCert, err := tls.X509KeyPair(certPEM, keyPEM)
-	if err != nil {
-		panic(err)
-	}
-	return &tls.Config{Certificates: []tls.Certificate{tlsCert}}
 }
